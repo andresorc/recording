abaecbe2fd93bd6de96d2824ffeda6f7dc87234f
Andres <andrescidor@gmail.com>
2024-12-02 18:46:04 +0100

    "Aplicacion actualizada con base de datos SQL incluida"

---
+++ models/store.py
@@ -0,0 +1,8 @@
+from db import db
+
+class StoreModel(db.Model):
+    __tablename__= "stores"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)  
+    items = db.relationship("ItemModel", back_populates="store", lazy="dynamic") #lazy="dynamic" gives the client the chance to make a query or not in order to get the items
---
+++ resources/store.py
@@ -0,0 +1,47 @@
+from sqlite3 import IntegrityError
+import uuid
+from flask import Flask, request
+from flask.views import MethodView
+from flask_smorest import Blueprint, abort
+from schemas import StoreSchema
+from models import StoreModel
+from sqlalchemy.exc import SQLAlchemyError
+from db import db
+
+blp = Blueprint("stores", __name__, description="Operations on stores")
+
+@blp.route("/store/<string:store_id>")
+class Store(MethodView):
+    @blp.response(200, StoreSchema)
+    def get(self, store_id):
+        store = StoreModel.query.get_or_404(store_id)
+        return store
+
+    def delete(self, store_id):
+       store = StoreModel.query.get_or_404(store_id)
+       db.session.delete(store)
+       db.session.commit()
+       return {"message": "Store deleted"}
+
+
+
+@blp.route("/store")
+class StoreList(MethodView):
+    @blp.response(200, StoreSchema)
+    def get(self):
+        return StoreModel.query.all()
+    
+    @blp.arguments(StoreSchema)
+    @blp.response(201, StoreSchema)
+    def post(self, store_data):
+        store = StoreModel(**store_data)
+
+        try:
+            db.session.add(store) # it prepares the item to be added to the database
+            db.session.commit()  # it actually adds the item to the database
+        except IntegrityError:
+            abort(400, message="A store with that name already exists.") 
+        except SQLAlchemyError:
+            abort(500, message="An error occurred while inserting the item.") 
+
+        return store
